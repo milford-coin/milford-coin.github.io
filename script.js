c0b5fd6be2226c8e7ab97fc2533525ce89b3e0a6
Aydyn <101376168+milford-coin@users.noreply.github.com>
2023-11-14 17:57:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-$(document).ready(function() {
-    // Функция для генерации случайного курса
-    function generateRandomRate() {
-        return (Math.random() * (1 - 0.5) + 0.5).toFixed(2);
-    }
+// Подключение jQuery
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.4.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
 
-    // Функция для обновления курса каждые 5 секунд
-    function updateExchangeRate() {
-        // Генерируем случайный курс
-        var newRate = generateRandomRate();
+// Ожидание загрузки jQuery
+script.onload = function() {
+    // Ваш оригинальный код
+    $(document).ready(function() {
+        // Функция для генерации случайного курса
+        function generateRandomRate() {
+            return (Math.random() * (1 - 0.5) + 0.5).toFixed(2);
+        }
 
-        // Обновляем значение на странице
-        $('#exchange-rate').text('Новый курс валюты: ' + newRate + ' долларов');
+        // Функция для обновления курса каждые 5 секунд
+        function updateExchangeRate() {
+            // Генерируем случайный курс
+            var newRate = generateRandomRate();
 
-        // Запускаем обновление каждые 5 секунд
-        setTimeout(updateExchangeRate, 5000);
-    }
+            // Обновляем значение на странице
+            $('#exchange-rate').text('Новый курс валюты: ' + newRate + ' долларов');
 
-    // Запускаем обновление курса
-    updateExchangeRate();
-});
+            // Запускаем обновление каждые 5 секунд
+            setTimeout(updateExchangeRate, 5000);
+        }
+
+        // Запускаем обновление курса
+        updateExchangeRate();
+    });
+};
