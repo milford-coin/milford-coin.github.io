0124c8516b78a48226c607f1bf3fc7dbc917b057
Aydyn <101376168+milford-coin@users.noreply.github.com>
2023-11-14 17:53:02 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // Функция для генерации случайного курса
+    function generateRandomRate() {
+        return (Math.random() * (1 - 0.5) + 0.5).toFixed(2);
+    }
+
+    // Функция для обновления курса каждые 5 секунд
+    function updateExchangeRate() {
+        // Генерируем случайный курс
+        var newRate = generateRandomRate();
+
+        // Обновляем значение на странице
+        $('#exchange-rate').text('Новый курс валюты: ' + newRate + ' долларов');
+
+        // Запускаем обновление каждые 5 секунд
+        setTimeout(updateExchangeRate, 5000);
+    }
+
+    // Запускаем обновление курса
+    updateExchangeRate();
+});
